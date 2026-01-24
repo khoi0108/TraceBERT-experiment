3d4f45143d47fd06334556d02ebcac1f1cdc6817
khoiha0108 <hadangkhoi2006@gmail.com>
2026-01-24 17:34:56 +0200

    Added Risha's evaluation toolkit

---
+++ trace/evaluation/single_model/metrices.py
@@ -0,0 +1,222 @@
+# type: ignore
+import os
+import pandas as pd
+from pandas import DataFrame
+from sklearn.metrics import precision_recall_curve, PrecisionRecallDisplay
+import matplotlib.pyplot as plt
+from args import get_eval_args
+import numpy as np
+import json
+
+args = get_eval_args()
+
+class Metrices:
+    def __init__(self, args, df):
+        self.fixed_threshold = 0.01
+        self.data_frame = self.add_labels_and_types(df)
+        self.output_dir = args.output_dir
+        self.data_dir = os.path.join(args.root_data_dir, args.repo_path)
+        self.iss_ids, self.cm_ids, self.pred, self.label, self.type = df['issue_id'], df['commit_id'], df['prediction'], df['label'], df['link_type']
+        self.group_sort = None
+        self.confusion_metrices = {'tp':[], 'fp':[], 'tn':[], 'fn':[]}
+
+    def read_json_file(self, file_name):
+        file_path = os.path.join(args.root_data_dir, args.repo_path, file_name)
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+        return data
+
+    def add_labels_and_types(self, result_df):
+        iss_ids, cm_ids, predictions = result_df['issue_id'], result_df['commit_id'], result_df['prediction']
+
+        t1_link, t2_link, t3_link = self.read_json_file('t1.json'), self.read_json_file('t2.json'), self.read_json_file('t3.json')
+        
+        result_df['link_type'] = 4
+        result_df['label'] = 0
+
+
+        for idx, (iss, cm) in enumerate(zip(iss_ids, cm_ids)):
+            issue = str(iss)
+            if issue in t1_link:
+                commits = set(t1_link[issue])
+                if cm in commits:
+                    result_df.at[idx, 'link_type'] = 1
+                    result_df.at[idx, 'label'] = 1
+                    continue
+            if issue in t2_link:
+                commits = set(t2_link[issue])
+                if cm in commits:
+                    result_df.at[idx, 'link_type'] = 2
+                    continue
+            if issue in t3_link:
+                commits = set(t3_link[issue])
+                if cm in commits:
+                    result_df.at[idx, 'link_type'] = 3
+
+        result_df = result_df.sort_values(by=['link_type', 'prediction'], ascending=[True, False], ignore_index = True)
+        positive_df = result_df[result_df['prediction'] > self.fixed_threshold]
+        negative_df = result_df[result_df['prediction'] <= self.fixed_threshold]
+        positive_df.to_csv(os.path.join(args.output_dir, 'positives.csv'), index=False)
+        negative_df.to_csv(os.path.join(args.output_dir, 'negatives.csv'), index=False)  
+        return result_df
+
+    def f1_score(self, precision, recall):
+        return 2 * (precision * recall) / (precision + recall) if precision + recall > 0 else 0
+
+    def f2_score(self, precision, recall):
+        return 5 * precision * recall / (4 * precision + recall) if precision + recall > 0 else 0
+    
+
+    def sort_df(self):
+        issue_df = pd.read_csv(os.path.join(self.data_dir, 'clean_issue.csv'))
+        commit_df = pd.read_csv(os.path.join(self.data_dir, 'clean_commit.csv'))
+
+        confusion_directory = os.path.join(self.output_dir, 'confusion')
+        if not os.path.exists(confusion_directory):
+            os.makedirs(confusion_directory)
+
+        for item in self.confusion_metrices:
+            df = pd.DataFrame(self.confusion_metrices[item], columns=['issue_id', 'commit_id', 'prediction', 'label'])
+            merged_df = pd.merge(df, issue_df, on='issue_id', how='left')
+            merged_df = pd.merge(merged_df, commit_df, on='commit_id', how='left')
+            final_df = merged_df[['issue_id', 'commit_id', 'prediction', 'label', 'issue_desc', 'issue_comments', 'summary', 'diff_added', 'diff_removed']]
+            final_df.to_csv(os.path.join(confusion_directory, item + '.csv'), index=False)
+
+    def f1_details(self, threshold):
+        "Return true positive (tp), fp, tn,fn "
+
+        tp, fp, tn, fn = 0, 0, 0, 0
+        for iss_id, cm_id, pred, label in zip(self.iss_ids, self.cm_ids, self.pred, self.label):
+            pred_value = pred
+            if pred > threshold:
+                pred = 1
+            else:
+                pred = 0
+            if pred == label:
+                if label == 1:
+                    tp += 1
+                    self.confusion_metrices['tp'].append((iss_id, cm_id, pred_value, label))
+                else:
+                    tn += 1
+                    self.confusion_metrices['tn'].append((iss_id, cm_id, pred_value, label))
+            else:
+                if label == 1:
+                    fn += 1
+                    self.confusion_metrices['fn'].append((iss_id, cm_id, pred_value, label))
+                else:
+                    fp += 1
+                    self.confusion_metrices['fp'].append((iss_id, cm_id, pred_value, label))
+        self.sort_df()
+        return {"True Positive": tp, "False Positive": fp, "True Negative": tn, "False Negative": fn}
+    
+    def get_precision_recall_curve(self, fig_name):
+        precision, recall, thresholds = precision_recall_curve(self.label, self.pred)
+        max_f1 = 0
+        max_f2 = 0
+        max_threshold = 0
+        for p, r, tr in zip(precision, recall, thresholds):
+            f1 = self.f1_score(p, r)
+            f2 = self.f2_score(p, r)
+            if f1 >= max_f1:
+                max_f1 = f1
+                max_threshold = tr
+            if f2 >= max_f2:
+                max_f2 = f2
+        viz = PrecisionRecallDisplay(
+            precision=precision, recall=recall)
+        viz.plot()
+        if os.path.isdir(self.output_dir):
+            fig_path = os.path.join(self.output_dir, fig_name)
+            plt.savefig(fig_path)
+            plt.close()
+        # we are not using the max_threshold calculated above, instead we are using a fixed threshold for evaluation.
+        detail = self.f1_details(self.fixed_threshold)
+        return round(max_f1, 3), round(max_f2, 3), detail, self.fixed_threshold
+
+    def precision_at_K(self, k=1):
+        if self.group_sort is None:
+            self.group_sort = self.data_frame.groupby(["issue_id"]).apply(
+                lambda x: x.sort_values(["prediction"], ascending=False)).reset_index(drop=True)
+        group_tops = self.group_sort.groupby('issue_id')
+        cnt = 0
+        hits = 0
+        for iss_id, group in group_tops:
+            for index, row in group.head(k).iterrows():
+                hits += 1 if row['label'] == 1 else 0
+            cnt += 1
+        return round(hits / cnt if cnt > 0 else 0, 3)
+    
+    def MAP_at_K(self, k=1):
+        if self.group_sort is None:
+            self.group_sort = self.data_frame.groupby(["issue_id"]).apply(
+                lambda x: x.sort_values(["prediction"], ascending=False)).reset_index(drop=True)
+        group_tops = self.group_sort.groupby('issue_id')
+        ap_sum = 0
+        for iss_id, group in group_tops:
+            group_hits = 0
+            ap = 0
+            for i, (index, row) in enumerate(group.head(k).iterrows()):
+                if row['label'] == 1:
+                    group_hits += 1
+                    ap += group_hits / (i + 1)
+            ap = ap / group_hits if group_hits > 0 else 0
+            ap_sum += ap
+        map = ap_sum / len(group_tops) if len(group_tops) > 0 else 0
+        return round(map, 3)
+    
+    def MRR(self):
+        if self.group_sort is None:
+            self.group_sort = self.data_frame.groupby(["issue_id"]).apply(
+                lambda x: x.sort_values(["prediction"], ascending=False)).reset_index(drop=True)
+        group_tops = self.group_sort.groupby('issue_id')
+        mrr_sum = 0
+        for iss_id, group in group_tops:
+            rank = 0
+            for i, (index, row) in enumerate(group.iterrows()):
+                rank += 1
+                if row['label'] == 1:
+                    mrr_sum += 1.0 / rank
+                    break
+        return mrr_sum / len(group_tops)
+    
+    def get_all_metrices(self):
+        pk3 = self.precision_at_K(3)
+        pk2 = self.precision_at_K(2)
+        pk1 = self.precision_at_K(1)
+        
+        best_f1, best_f2, details, f1_threshold = self.get_precision_recall_curve("pr_curve.png")
+        map = self.MAP_at_K(3)
+        mrr = self.MRR()
+        return {
+            'pk3': pk3,
+            'pk2': pk2,
+            'pk1': pk1,
+            'best_f1': best_f1,
+            'best_f2': best_f2,
+            'MAP': map,
+            'MRR': mrr,
+            'confusion_metrices': details,
+            'f1_threshold': f1_threshold
+        }
+    
+
+    def write_summary(self, exe_time=None):
+        summary_path = os.path.join(self.output_dir, "summary.json")
+        result = self.get_all_metrices()
+        if exe_time:
+            result['execution_time'] = exe_time
+        with open(summary_path, 'w') as file:
+            import json
+            json.dump(result, file, indent=4)
+        print(result)
+
+
+if __name__=="__main__":
+    args = get_eval_args()
+    res_file = os.path.join(args.output_dir, "raw_res.csv")
+    print(res_file)
+    if os.path.isfile(res_file):
+        result_df = pd.read_csv(res_file)
+        metrices = Metrices(args, df=result_df)
+        metrices.write_summary()
+
---
+++ trace/evaluation/single_model/single_eval.py
@@ -0,0 +1,185 @@
+# type: ignore
+import logging
+import os
+import sys
+import time
+from tqdm import tqdm
+import pandas as pd
+import json
+import math
+
+import torch
+from transformers import BertConfig
+
+from torch.utils.data import DataLoader
+
+# from metrices import metrics
+# from utils import results_to_df, format_batch_input_for_single_bert
+sys.path.append("..")
+sys.path.append("../..")
+
+from args import get_eval_args
+from models import TBertS
+from metrices import Metrices
+from git_repo_collector import Issues, Commits
+
+class Test:
+    def __init__(self):
+        self.data_dir = os.path.join(args.root_data_dir, args.repo_path)
+        self.issue_collection = Issues()
+        self.commit_collection = Commits()
+
+    # def find_link(self, iss_id, cm_id, links):
+    #     if str(iss_id) in links:
+    #         if cm_id in set(links[str(iss_id)]):
+    #             return 1
+    #     return 0
+    
+    def result_to_df(self, res):
+        df = pd.DataFrame()
+        df['issue_id'] = [x[0] for x in res]
+        df['commit_id'] = [x[1] for x in res]
+        df['prediction'] = [x[2] for x in res]
+        return df
+    
+    def read_artifacts(self, file_path, type):
+        df = pd.read_csv(file_path, keep_default_na=False)
+        if type == 'commit':
+            for _ , row in df.iterrows():
+                self.commit_collection.add_commit(commit_id=row['commit_id'], summary=row['summary'], diff_added=row['diff_added'], 
+                                                  diff_removed=row['diff_removed'], files=row['files'], commit_time=row['commit_time'])
+            return self.commit_collection.get_all_commits_map()
+        else:
+            for _ , row in df.iterrows():
+                self.issue_collection.add_issue(number=row['issue_id'], body=row['issue_desc'], comments=row['issue_comments'], 
+                                                createdAt=row['created_at'], updatedAt=row['closed_at'])
+            return self.issue_collection.get_all_issues_map()
+    
+    def store_artifact_text(self, issue_id_list, commit_id_list):
+        self.issue_text_map = {}
+        self.commit_text_map = {}
+        for iss_id in issue_id_list:
+            issue = self.issues[iss_id]
+            iss_text = f"{issue['issue_desc']} {issue['issue_comments']}"  
+            self.issue_text_map[iss_id] = iss_text.strip()
+        for cm_id in commit_id_list:
+            commit = self.commits[cm_id]
+            cm_text = f"{commit['summary']} ADDED_LINES {commit['diff_added']} REMOVED_LINES {commit['diff_removed']}"
+            self.commit_text_map[cm_id] = cm_text.strip()
+
+    def get_chunked_retrival_examples(self):
+        commit_file = os.path.join(self.data_dir, "clean_commit.csv")
+        issue_file = os.path.join(self.data_dir, "clean_issue.csv")
+
+        self.issues = self.read_artifacts(issue_file, type="issue")
+        self.commits = self.read_artifacts(commit_file, type="commit")
+
+        issue_id_list = self.issues.keys()
+        commit_id_list = self.commits.keys()
+
+        self.store_artifact_text(issue_id_list, commit_id_list)
+
+        examples = []
+        for iss_id in issue_id_list:
+            for cm_id in commit_id_list:
+                # label = self.find_link(iss_id, cm_id, links)
+                examples.append((iss_id, cm_id))
+        return examples
+
+    def format_batch_input(self, batch, model):
+        tokenizer = model.tokenizer
+        iss_ids, cm_ids = batch[0].tolist(), batch[1]
+        input_ids = []
+        att_masks = []
+        tk_types = []
+        for iss_id, cm_id in zip(iss_ids, cm_ids):
+            iss_text, cm_text = self.issue_text_map[iss_id], self.commit_text_map[cm_id]
+
+            feature = tokenizer.encode_plus(
+                text=iss_text,
+                text_pair=cm_text,
+                padding = 'max_length',
+                truncation = True,
+                return_attention_mask=True,
+                return_token_type_ids=True,
+                max_length=512,
+                add_special_tokens=True
+            )
+            input_ids.append(torch.tensor(feature["input_ids"], dtype=torch.long))
+            att_masks.append(torch.tensor(feature["attention_mask"], dtype=torch.long))
+            tk_types.append(torch.tensor(feature["token_type_ids"], dtype=torch.long))
+        input_tensor = torch.stack(input_ids)
+        att_tensor = torch.stack(att_masks)
+        tk_type_tensor = torch.stack(tk_types)
+        features = [input_tensor, att_tensor, tk_type_tensor]
+        features = [t.to(model._device) for t in features]
+        inputs = {
+            'input_ids': features[0],
+            'attention_mask': features[1],
+            'token_type_ids': features[2],
+        }
+        return inputs
+
+    def test(self, args, model, res_file_path):
+        # get (issue_id, commit_id, label) array and store in retrival_examples
+        chunked_examples = self.get_chunked_retrival_examples()
+        retrival_dataloader = DataLoader(chunked_examples, batch_size = args.per_gpu_eval_batch_size)
+
+        res = []
+        for batch in tqdm(retrival_dataloader, desc="retrival evaluation"):
+            iss_ids = batch[0]
+            cm_ids = batch[1]
+            with torch.no_grad():
+                model.eval()
+                inputs = self.format_batch_input(batch, model)
+                sim_score = model.get_sim_score(**inputs)
+                for n, p, prd in zip(iss_ids.tolist(), cm_ids, sim_score):
+                    res.append((n, p, prd))
+        df = self.result_to_df(res)        
+        df.to_csv(res_file_path, index=False)
+        return df
+
+if __name__ == "__main__":
+    args = get_eval_args()
+    logging.basicConfig(level='INFO')
+    logger = logging.getLogger(__name__)
+
+    device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
+    res_file = os.path.join(args.output_dir, "raw_res.csv")
+    exe_time = None
+
+    if os.path.isfile(res_file) and not args.overwrite:
+        logger.info('Evaluation result already exists')
+        result_df = pd.read_csv(res_file)
+    else:
+        if not os.path.isdir(args.output_dir):
+            os.makedirs(args.output_dir)
+        # if not os.path.isdir('./cache'):
+        #     os.makedirs('./cache')
+
+        # model_cache_file = os.path.join('./cache/', 'single_model_cache.pt')
+        # if os.path.isfile(model_cache_file):
+        #     model = TBertS(BertConfig(), args.code_bert, device=device)
+        #     model.load_state_dict(torch.load(model_cache_file, map_location=device))
+        # else:
+        #     model = TBertS(BertConfig(), args.code_bert, device=device)
+        #     torch.save(model.state_dict(), model_cache_file)
+
+        if args.model_path and os.path.exists(args.model_path):
+            model_path = os.path.join(args.model_path, 't_bert.pt')
+            model = TBertS(BertConfig(), args.code_bert, device=device, use_lora=False) # use_lora=True if you want to use lora fine-tuned model 
+            model.load_state_dict(torch.load(model_path, map_location=torch.device(device)), strict=False)
+        else:
+            raise Exception("evaluation model not found")
+        logger.info("model loaded")
+
+        start_time = time.time()
+        test_dir = os.path.join(args.root_data_dir, args.repo_path)
+        test = Test()
+        result_df = test.test(args, model, res_file)
+        exe_time = time.time() - start_time
+        logger.info("Execution time: " + str(exe_time))
+
+    metrices = Metrices(args, result_df)
+    metrices.write_summary(exe_time=exe_time)
+    
